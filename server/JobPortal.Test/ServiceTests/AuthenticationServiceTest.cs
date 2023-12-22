e0ce5d829522968e84e3eb29fe3fae239b3fcd48
Mert Gökmen <mertgkmeen@gmail.com>
2023-12-22 20:03:36 +0300

    added login test

--- server/JobPortal.Test/ServiceTests/AuthenticationServiceTest.cs
+++ server/JobPortal.Test/ServiceTests/AuthenticationServiceTest.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using JobPortal.Application.Interfaces;
+using JobPortal.Application.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FakeItEasy;
+using JobPortal.Controllers;
+using JobPortal.Application.ViewModels.ResponseM;
+using JobPortal.Application.ViewModels.AuthM;
+using FluentAssertions;
+using JobPortal.Domain.Entities;
 
 namespace JobPortal.Test.ServiceTests
 {
-    internal class AuthenticationServiceTest
+    public class AuthenticationServiceTest
     {
+        private readonly IAuthenticationService authenticationService;
+        private readonly IUserService userService;
+        private readonly IRoleService roleService;
+        private readonly IEmployerService employerService;
+
+        public AuthenticationServiceTest()
+        {
+            this.userService = A.Fake<IUserService>();
+            this.roleService = A.Fake<IRoleService>();
+            this.employerService = A.Fake<IEmployerService>();
+            this.authenticationService = A.Fake<IAuthenticationService>();
+        }
+
+        [Fact]
+        public void AuthenticationController_Login_ReturnsResponseViewModel()
+        {
+            var response = A.Fake<ResponseViewModel>();
+            var model = A.Fake<LoginModel>();
+            var user = A.Fake<User>();
+            var email = "savdasd";
+            A.CallTo(() =>  userService.getUserByEmail(email)).Returns(user);
+            var result = authenticationService.Login(model);
+
+            result.Should().BeOfType<ResponseViewModel>();
+        }
+
     }
 }
